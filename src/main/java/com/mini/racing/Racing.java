73fcef8b43684c98d125638d5daba775776e76fe
ls1925 <103xzy@gmail.com>
2021-04-13 15:20:42 +0800

    horse racing

---
+++ src/main/java/com/mini/racing/Racing.java
@@ -0,0 +1,11 @@
+package com.mini.racing;
+
+public class Racing {
+    public static void main(String[] args) {
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        h2.start();
+        h1.start();
+    }
+
+}
